8e0d84ffb7e3500e3857d1136cdb9f74d9c827ca
kalinom7 <kalinom7@DESKTOP-BTPED6M.localdomain>
2025-12-03 23:39:39 +0100

    add simple maze walls

--- script.js
+++ script.js
@@ -17,6 +17,48 @@ var map = [
     [1000,0,0,0,-90,0,2000,200,"patterns/brick_crosswalk_diff_1k.jpg"], // right wall
     [-1000,0,0,0,90,0,2000,200,"patterns/brick_crosswalk_diff_1k.jpg"], // left wall
     [0,100,0,90,0,0,2000,2000,"patterns/plank_flooring_04_diff_1k.jpg"],  // floor
+    [0,0,-1000,0,0,0,80,180,"patterns/door.png"], //door on front wall
+    [1000,50,0,0,-90,0,360,360,"patterns/window.png"], //window on right wall
+    
+
+// left vertical long
+[ -700, 0, 300, 0, 90, 0, 600, 200, "#0077ff" ],
+
+// top long horizontal
+[ 0, 0, -600, 0, 0, 0, 900, 200, "#0077ff" ],
+
+// right small top
+[ 700, 0, -300, 0, 0, 0, 150, 200, "#0077ff" ],
+
+// mid tall left
+[ -200, 0, -200, 0, 90, 0, 400, 200, "#0077ff" ],
+
+// middle small horizontal
+[ 150, 0, -100, 0, 0, 0, 350, 200, "#0077ff" ],
+
+// right vertical long
+[ 600, 0, 50, 0, 90, 0, 450, 200, "#0077ff" ],
+
+// bottom right horizontal
+[ 400, 0, 350, 0, 0, 0, 600, 200, "#0077ff" ],
+
+// bottom left horizontal
+[ -400, 0, 350, 0, 0, 0, 400, 200, "#0077ff" ],
+
+// small vertical bottom left
+[ -450, 0, 50, 0, 90, 0, 250, 200, "#0077ff" ],
+
+// lower middle vertical
+[ 50, 0, 200, 0, 90, 0, 250, 200, "#0077ff" ],
+
+// next to back wall
+[500,0,700,0,180,0,700,200,"#0077ff"],
+[-400,0,600,0,180,0,300,200,"#0077ff"],
+[-100,0,500,0,90,0,700,200,"#0077ff"],
+ [-600,0,-300,0,0,0,600,200,"#0077ff"],
+
+
+    
 ];
 
 //variables for movement 
@@ -127,6 +169,7 @@ function createNewWorld(){
         newElement.style.height = map[i][7] + "px";
         newElement.style.background = map[i][8];
         newElement.style.backgroundImage = "url("+ map[i][8] +")";
+        
 
         //position
         newElement.style.transform = "translate3d(" 
