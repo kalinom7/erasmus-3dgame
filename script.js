473c96aa08fd3fc627a2a8631480d85141f946ef
kalinom7 <kalinom7@DESKTOP-BTPED6M.localdomain>
2025-12-03 11:24:55 +0100

    add mouse rotation movement

--- script.js
+++ script.js
@@ -1,55 +1,5 @@
-//variables for movements
-let PressLeft = false;
-let PressRight = false;
-let PressForward = false;
-let PressBack = false;
-let PressUp = false;
-let MouseX = true;
-let MouseY = false;
-
-//if the key is pressed
-document.addEventListener('keydown', (event) => {
-    if( event.key == "w" || event.key == "ArrowUp"){
-        PressForward = true;
-    }
-    if( event.key == "s" || event.key == "ArrowDown"){
-        PressBack = true;
-    }
-    if( event.key == "a" || event.key == "ArrowLeft"){
-        PressLeft = true;
-    }
-    if( event.key == "d" || event.key == "ArrowRight"){
-        PressRight = true;
-    }
-    if ( event.key == " "){
-        PressUp = true;
-    }
-
-})
-
-//if the key is released
-document.addEventListener('keyup', (event) => {
-    if( event.key == "w" || event.key == "ArrowUp"){
-        PressForward = false;
-    }
-    if( event.key == "s" || event.key == "ArrowDown"){
-        PressBack = false;
-    }
-    if( event.key == "a" || event.key == "ArrowLeft"){
-        PressLeft = false;
-    }
-    if( event.key == "d" || event.key == "ArrowRight"){
-        PressRight = false;
-    }
-    if ( event.key == " "){
-        PressUp = false;
-    }
-})
-
-document.addEventListener('mousemove', (event) => {
-    MouseX = event.movementX;
-    MouseY = event.movementY;
-})
+//world constant 
+var deg = Math.PI/180;
 
 
 function player(x,y,z,rx,ry){
@@ -60,33 +10,123 @@ function player(x,y,z,rx,ry){
     this.ry = ry;
 }
 
-let pawn = new player(0,0,0,0,0);
-let world = document.getElementById("world");
+//variables for movement 
+var PressLeft = 0;
+var PressRight = 0;
+var PressForward = 0;
+var PressBack = 0;
+var PressUp = 0;
+var MouseX = 0;
+var MouseY = 0;
+var lock = false;
+
+//link variable to container
+var container = document.getElementById("container");
+//if the mouse is pressed
+
+container.onclick = function(){
+    container.requestPointerLock();
+}
+
+//locked mouse listener
+document.addEventListener('pointerlockchange', (event) =>{
+    lock = !lock;
+})
+//if the key is pressed 
+document.addEventListener("keydown", (event) => {
+    if (event.key == "w"){
+        PressForward = 1;
+    }
+    if (event.key == "s"){
+        PressBack = 1;
+    }
+    if (event.key == "d"){
+        PressRight = 1;
+    }
+    if (event.key == "a"){
+        PressLeft = 1;
+    }
+    if (event.key == " "){
+        PressUp = 1;
+    }
+})
+
+//if the key is relealised 
+document.addEventListener("keyup", (event) =>{
+    if (event.key == "w"){
+        PressForward = 0;
+    }
+    if (event.key == "s"){
+        PressBack = 0;
+    }
+    if (event.key == "d"){
+        PressRight = 0;
+    }
+    if (event.key == "a"){
+        PressLeft = 0;
+    }
+    if (event.key == " "){
+        PressUp = 0;
+    }
+})
+document.addEventListener("keydown", function(event) {
+         if (event.key == "ArrowLeft"){
+            PressLeft = 1;
+         } else if (event.key == "ArrowUp"){
+            PressForward = 1;
+         } else if (event.key == "ArrowRight"){
+            PressRight = 1;
+         } else if (event.key == "ArrowDown"){
+            PressBack = 1;
+         }
+      });
+
+document.addEventListener("keyup", function(event) {
+         if (event.key == "ArrowLeft"){
+            PressLeft = 0;
+         } else if (event.key == "ArrowUp"){
+            PressForward = 0;
+         } else if (event.key == "ArrowRight"){
+            PressRight = 0;
+         } else if (event.key == "ArrowDown"){
+            PressBack = 0;
+         }
+      });
+    
+// mouse movement listener 
+document.addEventListener("mousemove", (event) =>{
+    MouseX = event.movementX;
+    MouseY = event.movementY;
+}) 
+
+var pawn = new player(0,0,0,0,0);
+var world = document.getElementById("world");
 
 function update(){
-    //count movement
-    dx = PressRight - PressLeft;
-    dz = PressBack - PressForward;
-    dy = PressUp;
+    //count movement 
+    dx = Math.cos(pawn.ry * deg) * (PressRight - PressLeft) - Math.sin(pawn.ry * deg) * (PressForward - PressBack); 
+    dz = - (Math.sin(pawn.ry * deg) * (PressRight - PressLeft) + Math.cos(pawn.ry * deg) * (PressForward - PressBack));
+    dy = -PressUp;
     drx = MouseY;
     dry = - MouseX;
-    MouseY =0;
-    MouseX =0;
+    MouseX = MouseY = 0;
 
-    
-    //add movement to the coordinates
+    //add movement to the coordinate 
     pawn.x = pawn.x + dx;
     pawn.y = pawn.y + dy;
     pawn.z = pawn.z + dz;
-    pawn.rx = pawn.rx + drx;
-    pawn.ry = pawn.ry + dry;
+    if(lock){
+        pawn.rx = pawn.rx + drx;
+        pawn.ry = pawn.ry + dry;
+    }
+   
 
-
-    //change coordinates of the world
-    world.style.transform = "translateZ(600px)"
-    +"rotateX(" + (-pawn.rx) + "deg)" 
-    +"rotateY(" + (-pawn.ry) + "deg)"
-    + "translate3d(" + (-pawn.x) +"px," + (-pawn.y) +"px," + (-pawn.z) +"px)";
+    
+    //change coordinate of the world 
+    world.style.transform = "translateZ(600px)" + 
+        "rotateX(" + (-pawn.rx) + "deg)" +
+        "rotateY(" + (-pawn.ry) + "deg)" +
+        "translate3d(" + (-pawn.x) +"px," + (-pawn.y) + "px," + (-pawn.z) + "px)";
 }
 
 TimerGame = setInterval(update, 10);
